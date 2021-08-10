1f52cc91a3cb6c0d688e6dec1b99a6a962bf7d98
simonjheiler <simon.heiler@uni-bonn.de>
2021-08-10 17:56:25 +0200

    Add first task.

--- src/data_management/task_data_management.py
+++ src/data_management/task_data_management.py
@@ -3,17 +3,6 @@ import pytask
 from src.config import BLD
 from src.config import SRC
 
-# data = SRC / "original_data" / "ajrcomment.dta"
-# prod = BLD / "data" / "ajrcomment_all.csv"
-#
-#
-# @pytask.mark.r([str(x.resolve()) for x in [data, prod]])
-# @pytask.mark.depends_on(["add_variables.r", data])
-# @pytask.mark.produces(prod)
-# def task_ajr_comment_data():
-#     pass
-
-
 # datasets = [
 #     "SCFP1989.csv",
 #     "SCFP1992.csv",
