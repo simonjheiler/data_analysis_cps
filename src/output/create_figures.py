8cf9f94c50a0a5381721ec2e097b1b678cd7b519
simonjheiler <simon.heiler@uni-bonn.de>
2022-10-10 16:02:43 +0200

    Add estimation of returns to tenure and creation of outputs to pytask.

--- src/output/create_figures.py
+++ src/output/create_figures.py
@@ -26,7 +26,7 @@ from src.utilities.plot_utils import _plot_lines_with_error_bars
 idx = pd.IndexSlice
 
 tenure = pd.read_csv(
-    BLD / "out" / "results" / "cps_returns_to_tenure.csv", index_col=["x", "group"]
+    BLD / "results" / "cps_returns_to_tenure.csv", index_col=["x", "group"]
 )
 
 
@@ -65,7 +65,7 @@ def create_figures():
         ],
         "plot_error_bars": False,
         "plot_ci": True,
-        "outpath": BLD / "out" / "figures" / "cps_earnings_tenure_education.pdf",
+        "outpath": BLD / "figures" / "cps_earnings_tenure_education.pdf",
     }
     _plot_lines_with_error_bars(plot_data, plot_params)
 
--- src/output/task_output.py
+++ src/output/task_output.py
@@ -7,8 +7,8 @@ import pytask
 from src.config import BLD
 from src.output.create_figures import create_figures
 
-data = BLD / "out" / "results" / "cps_returns_to_tenure.csv"
-prod = BLD / "out" / "figures" / "cps_earnings_tenure_education.pdf"
+data = BLD / "results" / "cps_returns_to_tenure.csv"
+prod = BLD / "figures" / "cps_earnings_tenure_education.pdf"
 
 
 @pytask.mark.depends_on(data)
